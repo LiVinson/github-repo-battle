746ad240ecd24fba2cf6860b1a9669262dfb4c61
Lisa Vinson <Lrv0828@gmail.com>
2020-03-10 12:25:19 -0400

    Added navbar component with initial styling

--- app/index.js
+++ app/index.js
@@ -1,10 +1,14 @@
 import React from "react"
 import ReactDom from "react-dom"
+import Popular from "./components/Popular"
 import "./index.css"
-
 class App extends React.Component {
   render() {
-    return <div>Hello Lisa!</div>
+    return (
+      <div className="container">
+        <Popular />
+      </div>
+    )
   }
 }
 
