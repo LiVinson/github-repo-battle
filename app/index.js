ad683e401462dfd3c8cd9a4cf4a984fee04b1db1
Lisa Vinson <Lrv0828@gmail.com>
2020-03-17 17:31:12 -0400

    added instructions, playerinput, and Battle components. Added css for Popular and Battle screen components.

--- app/index.js
+++ app/index.js
@@ -2,12 +2,12 @@ import React from "react"
 import ReactDom from "react-dom"
 import Popular from "./components/Popular"
 import "./index.css"
-
+import Battle from "./components/Battle"
 class App extends React.Component {
   render() {
     return (
       <div className="container">
-        <Popular />
+        <Battle />
       </div>
     )
   }
