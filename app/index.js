3a3498d1b0b5689496cb489e7dfe9d12107ddcb2
Lisa Vinson <Lrv0828@gmail.com>
2020-03-23 19:52:13 -0400

    created Theme Context and added ThemeProvider component to App  with state to track theme

--- app/index.js
+++ app/index.js
@@ -3,12 +3,28 @@ import ReactDom from "react-dom"
 import Popular from "./components/Popular"
 import "./index.css"
 import Battle from "./components/Battle"
+import ThemeProvider from "./contexts/theme"
+
 class App extends React.Component {
+  constructor() {
+    super(props)
+    this.state = {
+      theme: "light",
+      toggleTheme: () => {
+        this.setState(({ theme }) => ({
+          theme: theme === "light" ? "dark" : "light"
+        }))
+      }
+    }
+  }
+
   render() {
     return (
-      <div className="container">
-        <Battle />
-      </div>
+      <ThemeProvider value={this.state}>
+        <div className="container">
+          <Battle />
+        </div>
+      </ThemeProvider>
     )
   }
 }
