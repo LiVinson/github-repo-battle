8a6bff1bb588de79ff6e8b22083fd68b2d0d408a
Lisa Vinson <Lrv0828@gmail.com>
2020-03-22 20:18:31 -0400

    abstracted card logic for Result and Popular into its own Card component

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -8,6 +8,8 @@ import {
   FaExclamationTriangle
 } from "react-icons/fa"
 
+import Card from "./Card"
+
 //Takes in language user selected and a function to call when a new language is selected.
 function LanguagesNav({ selectedLanguage, onUpdateLanguage }) {
   const languages = ["All", "JavaScript", "Ruby", "Java", "CSS", "Python"]
@@ -51,37 +53,31 @@ function ReposGrid({ repos }) {
 
         return (
           <li key={html_url} className="card bg-light">
-            <h4 className="header-lg center-text">#{index + 1}</h4>
-            <img
-              className="avatar"
-              src={avatar_url}
-              alt={`Avatar for ${login}`}
-            />
-
-            <h2 className="center-text">
-              <a className="link" href={html_url}>
-                {login}
-              </a>
-            </h2>
-
-            <ul className="card-list">
-              <li>
-                <FaUser color="rgb(255, 191, 116)" size={22} />
-                <a href={`htps://github.com/${login}`}>{login}</a>
-              </li>
-              <li>
-                <FaStar color="rgb(255, 215, 0)" size={22} />
-                {stargazers_count.toLocaleString()}
-              </li>
-              <li>
-                <FaCodeBranch color="rgb(129, 195, 245)" size={22} />
-                {forks.toLocaleString()} forks
-              </li>
-              <li>
-                <FaExclamationTriangle color="rgb(241, 138, 147)" size={22} />
-                {open_issues.toLocaleString()} open issues
-              </li>
-            </ul>
+            <Card
+              header={`#${index + 1}`}
+              avater={avatar_url}
+              href={html_url}
+              name={login}
+            >
+              <ul className="card-list">
+                <li>
+                  <FaUser color="rgb(255, 191, 116)" size={22} />
+                  <a href={`htps://github.com/${login}`}>{login}</a>
+                </li>
+                <li>
+                  <FaStar color="rgb(255, 215, 0)" size={22} />
+                  {stargazers_count.toLocaleString()}
+                </li>
+                <li>
+                  <FaCodeBranch color="rgb(129, 195, 245)" size={22} />
+                  {forks.toLocaleString()} forks
+                </li>
+                <li>
+                  <FaExclamationTriangle color="rgb(241, 138, 147)" size={22} />
+                  {open_issues.toLocaleString()} open issues
+                </li>
+              </ul>
+            </Card>
           </li>
         )
       })}
