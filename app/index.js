10b5d3f5d3cb15875eafc1f7cdefa85afd453150
Lisa Vinson <Lrv0828@gmail.com>
2020-04-18 14:31:26 -0400

    refactored all components that used Consumer property of ThemeConsumer to use useContext Hook

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import ReactDom from "react-dom"
 import "./index.css"
-import { ThemeProvider } from "./contexts/theme"
+import ThemeContext from "./contexts/theme"
 import Nav from "./components/Nav"
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import Loading from "./components/Loading"
@@ -10,12 +10,9 @@ const Popular = React.lazy(() => import("./components/Popular"))
 const Battle = React.lazy(() => import("./components/Battle"))
 const Results = React.lazy(() => import("./components/Results"))
 
-function App2() {
-  //Track theme across renders.
-  //Rerender when theme changes.
-  //Have a function that modifies theme when called.
-  //Pass theme and toggleTheme function to child component(s)
+function App() {
   const [theme, setTheme] = React.useState("light")
+
   const value = React.useMemo(
     () => ({
       theme,
@@ -26,7 +23,7 @@ function App2() {
 
   return (
     <Router>
-      <ThemeProvider value={value}>
+      <ThemeContext.Provider value={value}>
         <div className={theme}>
           <div className="container">
             <Nav />
@@ -40,42 +37,9 @@ function App2() {
             </React.Suspense>
           </div>
         </div>
-      </ThemeProvider>
+      </ThemeContext.Provider>
     </Router>
   )
-}
-
-class App extends React.Component {
-  state = {
-    theme: "light",
-    toggleTheme: () => {
-      this.setState(({ theme }) => ({
-        theme: theme === "light" ? "dark" : "light",
-      }))
-    },
-  }
-
-  render() {
-    return (
-      <Router>
-        <ThemeProvider value={this.state}>
-          <div className={this.state.theme}>
-            <div className="container">
-              <Nav />
-              <React.Suspense fallback={<Loading />}>
-                <Switch>
-                  <Route exact path="/" component={Popular} />
-                  <Route exact path="/battle" component={Battle} />
-                  <Route path="/battle/results" component={Results} />
-                  <Route render={() => <h1>404</h1>} />
-                </Switch>
-              </React.Suspense>
-            </div>
-          </div>
-        </ThemeProvider>
-      </Router>
-    )
-  }
 }
 
 ReactDom.render(<App />, document.getElementById("app"))
